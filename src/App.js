8ae13b248706a586872f0b72cf62f869b83ba31b
jadenHong <62584142+jadenHong@users.noreply.github.com>
2020-08-13 02:17:00 -0600

    React: bulletin-board
    finished practice

--- src/App.js
+++ src/App.js
@@ -1,38 +1,66 @@
 import React, { useState, useEffect } from 'react';
+import { REQUEST_TYPE } from './Constants';
+import { URL } from './Constants';
 
 const App = () => {
   const [userSearch, setUserSearch] = useState('');
 
-  const [information, setInformation] = useState({
-    id: '',
-    passwd: '',
-    name: '',
-    gender: 'male',
-    email: '',
-  });
+
   const [userInfo, setUserInfo] = useState({
     name: '',
     gender: '',
     email: '',
   });
   const [custInfo, setCustInfo] = useState([]);
-  const { id, passwd, name, gender, email } = information;
+  const [updateInfo, setUpdateInfo] = useState({
+    id: '',
+    content: '',
+  });
+  // const { id, passwd, name, gender, email } = information;
 
   useEffect(() => {
-    console.log(information);
-  })
+    getAllData();
+  }, []);
+
+  const makeObj = (param) => {
+    return {
+      test: 1,
+      ...(param === 1 && { test2: 2 })
+    }
+    // if (param === 1) {
+    //   return {
+    //     test: 1,
+    //     test2: 2
+    //   }
+    // } else {
+    //   return {
+    //     test: 1
+    //   }
+    // }
+  }
+
+
+  const fetchAPI = async (url, method = REQUEST_TYPE.GET, body = undefined) => {
+    const fetchOpt = {
+      method,
+      headers: { 'Content-Type': 'application/json' },
+    }
+    if (method === REQUEST_TYPE.POST && body) {
+      fetchOpt.body = JSON.stringify(body)
+    }
+    const response = await fetch(url, fetchOpt);
+    const json = await response.json();
+    return json;
+  }
+
+
 
   // ***** 이름으로 검색해서 출력 *****
-  const sendDataToServer = (userSearch) => {
-    const URL = `http://localhost:8090/customer/search/`;
-    console.log(URL + userSearch);
-    fetch(`${URL}${userSearch}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const [userData] = data.map((info) => info)
-        console.log(userData);
-        setUserInfo({ name: userData.name, gender: userData.gender, email: userData.email });
-      })
+  const sendDataToServer = async (userSearch) => {
+    const data = await fetchAPI(`${URL.SEARCH}${userSearch}`);
+    const [userData] = data.map((info) => info)
+    console.log(userData);
+    setUserInfo({ name: userData.name, gender: userData.gender, email: userData.email });
   }
 
   const handleChange = (e) => {
@@ -47,80 +75,75 @@ const App = () => {
   // *********************************
 
 
-  // ***** 모든 정보 DB에서 출력 *****
 
-
-  const handleClick = () => {
-    const URL = `http://localhost:8090/customer/information/`;
-    fetch(URL)
-      .then((response) => response.json())
-      .then((data) => setCustInfo(data))
-  }
-  // **************************************
-
-
-  // ***** 조건 입력 후 DB에 Row 추가 *****
-  const handleChangeToSave = (e) => {
+  // userID, content 내용 입력후 update한다.
+  const handlChangeToUpdate = (e) => {
     const { value, name } = e.target;
-
-    // 이전 information 값들에 추가 해주는 것.
-    setInformation({
-      ...information,
+    setUpdateInfo({
+      ...updateInfo,
       [name]: value
     });
 
   }
-  const handleSubmitSave = (e) => {
-    console.log(information)
+
+
+  const handleSubmitUpdate = async (e) => {
     e.preventDefault();
-    const URL = `http://localhost:8090/customer/save/`;
-    fetch(URL, {
-      method: 'POST',
-      body: JSON.stringify(information),
-      headers: { 'Content-Type': 'application/json' },
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
+    const data = fetchAPI(URL.UPDATE, REQUEST_TYPE.POST, updateInfo);
+    getAllData(data);
   }
-  // **********************************************
+
+
+  // 데이터 다 가져오기
+  const getAllData = async () => {
+    const data = await fetchAPI(URL.GETALL)
+    setCustInfo(data);
+  }
+
+  // content 삭제!!!
+  const deleteInfo = async (id) => {
+    const path = URL.DELETE + id;
+    const data = await fetchAPI(path)
+    getAllData(data);
+  }
+
+
+
 
   return (
     <div>
+      {/* 선택한 이름의 유저 정보얻기 */}
       <form onSubmit={handleSubmitFind} className="find-customer">
         <input type="text" onChange={handleChange} value={userSearch} />
       </form>
 
-      <button onClick={handleClick}>AllInfo</button>
+      {/* 모든 유저정보 얻기 */}
       <div>
         {`Name: ${userInfo.name} Gender: ${userInfo.gender} Email: ${userInfo.email}`}
       </div>
-      <div>
-        <h2>Cust Information</h2>
-        {custInfo.map((info, index) => {
-          return (
-            <h3 key={index}>{`Name: ${info.name} Gender: ${info.gender} Email: ${info.email} Title: ${info.title} Content: ${info.content}`}</h3>
-          )
-        })}
-      </div>
 
-      <form className="save-user" onSubmit={handleSubmitSave} >
-        <label>User ID:</label>
-        <input type="text" onChange={handleChangeToSave} name="id" />
-        <label>Password:</label>
-        <input type="text" onChange={handleChangeToSave} name="passwd" />
-        <label>Name:</label>
-        <input type="text" onChange={handleChangeToSave} name="name" />
-        <label>Gender:</label>
-        <select onChange={handleChangeToSave} name="gender">
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
-        </select>
-        <label>email:</label>
-        <input type="text" onChange={handleChangeToSave} name="email" />
+
+
+      {/* 유저 이름으로 content Update하기 */}
+      <form onSubmit={handleSubmitUpdate} >
+        <h2>content 와 userID 를 입력하세요</h2>
+        <input type="text" onChange={handlChangeToUpdate} name="id" />
+        <input type="text" onChange={handlChangeToUpdate} name="content" />
         <input type="submit" />
       </form>
 
+      {/* id입력받고 해당 데이터 삭제 */}
+      {custInfo.map((data, index) => {
+        const { id, name, gender, email, title, content } = data;
+        return (
+          // button에 onClick 시에 함수에 인자가 들어가게 되면 무조건 콜백사용해서 해라 제발
+          <div key={index}>{`${id} - ${name} - ${gender} - ${email} - ${title} - ${content}`}<button onClick={() => deleteInfo(id)}>Delete</button></div>
+        )
+      })}
+
     </div>
+
+
   )
 }
 
---
+++ src/Constants.js
@@ -0,0 +1,13 @@
+export const REQUEST_TYPE = {
+    POST: 'POST',
+    GET: 'GET',
+    DELETE: 'DELETE',
+}
+
+export const URL = {
+    SEARCH: `http://localhost:8090/customer/search/`,
+    SAVE: `http://localhost:8090/customer/save/`,
+    UPDATE: `http://localhost:8090/customer/update/`,
+    GETALL: `http://localhost:8090/customer/information/`,
+    DELETE: `http://localhost:8090/customer/delete/`,
+}
